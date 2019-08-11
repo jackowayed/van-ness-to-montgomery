7622e69dea6b0af7b03cb05ed30563e8d9332078
Glitch (coal-driver) <none>
2019-08-11 04:52:42 +0000

    🗃✍️ Checkpoint ./server.py:59205/739

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-#import py_nextbus
 import nextbus
 import string
 import threading
 import time
-
-NONCE = 0
-#client = py_nextbus.NextBusClient(output_format='json', agency='sf-muni')
 
 class Times:
   def __init__(self):
@@ -19,20 +15,27 @@ class Times:
     
   def update_times(self):
     old_coming = self.coming
-    self.coming = [vid for _, vid, __ in nextbus.nextbus_stop_helper('sf-muni', 'L', '15419')]
+    self.coming = {vid for _, vid, __ in nextbus.nextbus_stop_helper('sf-muni', 'L', '15419')}
     for vid in old_coming:
       if vid not in self.coming:
         self.left_ts[vid] = time.time()
+        self.in_transit.add(vid)
+    arriving = {vid for _, vid, __ in nextbus.nextbus_stop_helper('sf-muni', 'L', '15731')}
+    for vid in in_transit:
+      if vid not in arriving:
+        self.in_transit.remove(vid)
         
-  def debug():
-    return "{} {} {} {}".format(self.coming, self.in_transit, self.left_ts, self.arrived_ts)
+        
+  def debug(self):
+    return "Coming: {}\nIn Transit: {}\nLeft: {}\n In Transit: {}\n".format(
+      self.coming, self.in_transit, self.left_ts, self.arrived_ts)
         
 TIMES = Times()
     
 
 @app.route("/")
 def hello():
-  return "Hello World! {}\n".format(NONCE)
+  return TIMES.debug().replace("\n", "<br/>")
 
 @app.route("/update-times", methods=["POST"])
 def update_times():
@@ -42,6 +45,10 @@ def update_times():
 @app.route("/debug")
 def debug():
   return TIMES.debug()
+
+@app.route("/debug.html")
+def debug_html():
+  return TIMES.debug().replace("\n", "<br/>")
   
 
 if __name__ == "__main__":
