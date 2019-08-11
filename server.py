0c8efe52e26e88548f0b880202bd18403e1f50b1
Glitch (flask-python3) <none>
2019-08-11 01:46:38 +0000

    🚩✌️ Checkpoint ./server.py:59205/391

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
+import threading
+import time
+
+nonce = 0
+
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return f"Hello World! {nonce}"
 
 if __name__ == "__main__":
   app.run()
+  
+  
+def update_times():
+  nonce += 1
+
+def update_times_recurring():
+  update_times()
+  time.sleep(20)
