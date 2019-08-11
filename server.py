a611518d48b853de4f65586c8fc40f09da4c8475
Glitch (flask-python3) <none>
2019-08-11 02:08:38 +0000

    🍗🏎 Checkpoint ./requirements.txt:59205/11 ./server.py:59205/114

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+import py_nextbus
 import string
 import threading
 import time
 
 NONCE = 0
+client = py_nextbus.NextBusClient(output_format='json', agency='sf-muni')
 
 @app.route("/")
 def hello():
